7015464ecbe46e9fa428cc0fbba10a472e1463cb
aybike <aybikesayar@gmail.com>
2022-10-02 17:38:23 +0300

    cmmt

--- app.js
+++ app.js
@@ -4,39 +4,45 @@ let fishP = document.querySelector(".fıshtype p ");
 let hrotate = document.querySelector(".rotate");
 let img2 = document.querySelector(`.inimg2`);
 let text2 = document.querySelector(`.intext2`);
-let fıshAll = document.querySelectorAll(".fıshtype")
+let fıshAll = document.querySelector(".type-of-fısh")
 
-document.addEventListener(`scroll`, (e)=>{
-    console.log((document.body.scrollTop))
+document.addEventListener(`scroll`, (e) => {
+     console.log((document.body.scrollTop))
 
 
-   if(document.body.scrollTop >= 0){
-        img.classList.add("inimg1");
-        text.classList.add("intext1");
-        hrotate.classList.add("hrotate");
-        img2.classList.add("newinimg2")
-        text2.classList.add("newintext2");
-        
-        console.log(fıshAll)
-       
-          
-          setTimeout(() => {
-               fıshAll.forEach((item) => {
-                    console.log(item)
-                    item.classList.add(`fısh1`)
-                    
-               }) ;
-             }, "2000")
+     if (document.body.scrollTop >= 0) {
+          img.classList.add("inimg1");
+          text.classList.add("intext1");
+          hrotate.classList.add("hrotate");
+          img2.classList.add("newinimg2")
+          text2.classList.add("newintext2");
+
+          console.log(fıshAll)
+
+
+          for (let i = 0; i < 5;) {
+               setTimeout(() => {
+                    console.log(i)
+                    fıshAll.children[i].classList.remove(`fıshtype`)
+                    console.log(fıshAll.children[i])
+                    fıshAll.children[i].classList.add(`fısh1`)
+                    console.log(new Date().toLocaleTimeString())
+               }, i * 1000)
+               i++
+
+          }
+
+
+
      }
-});  
+});
 
-          
-          //setInterval(function(){item.classList.add("fısh1")},1000)
-         
-         
-         
 
-     
+
+
+
+
+
 
 
 
@@ -48,13 +54,13 @@ document.addEventListener(`mousemove`, (e)=>{
         text.classList.add("intext1")
      }
 })*/
-let menü = false; 
+let menü = false;
 
-document.querySelector(".hmbrg").addEventListener("click",()=>{
-  
- 
-if(menü == false){
-     document.querySelector(".menübar").innerHTML = `
+document.querySelector(".hmbrg").addEventListener("click", () => {
+
+
+     if (menü == false) {
+          document.querySelector(".menübar").innerHTML = `
      <nav class="navbar bg-light fixed-top">
       <div class="offcanvas-body">
         <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
@@ -90,8 +96,8 @@ if(menü == false){
       </div>
      </nav>
      `
-     menü = true
-     }else {
+          menü = true
+     } else {
           document.querySelector(".menübar").innerHTML = ""
           menü = false
      }
